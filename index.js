ae7b82ba838d2710b3ea1fb2b370fcf9022f7ce5
mccarronmollye@gmail.com <mccarronmollye@gmail.com>
2018-08-01 01:12:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays(string) {
 return "Happy holidays!"
 }
 
-function happyHolidaysTo(you){
-  console.log(Happy holidays, ${you}!)
+function happyHolidaysTo(name){
+return "Happy holidays, "name"! "
 }
